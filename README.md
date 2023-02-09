8626ecf430bdc89ab74d88b54309fafebe6218af
Xedll <97981835+Xedll@users.noreply.github.com>
2023-02-09 16:57:11 +0300

    Alpha v0.3
    Added editing logic for notes (But w/o protection from html tags input). Slightly changed appearance.

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ This is my project which should be similar to notes. It's will be updating in so
 ## What can i do?
 At this moment you can only add some todo-things. All information will storage on local pc in browser, so if you open project in different browser, information also will be different.
 
-## Update History
-- Alpha v0.0.2:
+## Change logs
+- Alpha v0.3:
+  - Added editing logic for notes (But w/o protection from html tags input).
+  - Slightly changed appearance.
+- Alpha v0.2:
   - Added deleting logic for notes.
-- Alpha v0.0.1:
+- Alpha v0.1:
   - Added basic logic.
--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.onload = () => {
    const todoTitle = document.querySelector('.form__title')
    const todoDesc = document.querySelector('.form__desc')
    const nodeForStickers = document.querySelector('.stickers')
-
+   let counterForStickerEdit = 0;
    let taskList = [];
 
    if (localStorage.taskList) { //Render data
@@ -22,9 +22,12 @@ window.onload = () => {
    }
 
    function setStickerDataToHTML(sticker) {
-      return `<div data-id="${sticker.id}" class="sticker"><div class="sticker__title">${sticker.title}</div><div class="sticker__content">${sticker.content}</div><button class="sticker__delete">Delete</button></div>`
+      return `<div data-id="${sticker.id}" class="sticker"><div class="sticker__title">${sticker.title}</div><div class="sticker__content">${sticker.content}</div><div class="sticker__buttons"><button class="sticker__delete">Delete</button><button class="sticker__edit">Edit</button></div></div>`
    }
 
+   function stickerSetter(sticker, variable, data) {
+      sticker[variable] = data;
+   }
 
    function deleteSticker(sticker) {
       for (let item of taskList) {
@@ -36,7 +39,77 @@ window.onload = () => {
       showLocalData(taskList)
    }
 
+   function editSticker(sticker) {
+      let childrens = Array.from(sticker.children);
+
+      let title = null;
+      let content = null;
+      let buttons = null
+      let editButton = null;
+
+      for (let item of childrens) {
+         if (item.classList.contains('sticker__title')) {
+            title = item
+         }
+         if (item.classList.contains('sticker__content')) {
+            content = item
+         }
+         if (item.classList.contains('sticker__buttons')) {
+            buttons = Array.from(item.children)
+         }
+      }
+
+      for (let button of buttons) {
+         if (button.classList.contains('sticker__edit')) {
+            editButton = button;
+         }
+      }
+
+      let titleEdited = title.textContent;
+      let contentEdited = content.textContent;
+
+      if (counterForStickerEdit == 0) {
+
+         counterForStickerEdit = 1;
+
+         replaceTag(title, 'textarea')
+         replaceTag(content, 'textarea')
+
+         editButton.textContent = 'Apply'
+
+      } else if (counterForStickerEdit == 1) {
+
+         counterForStickerEdit = 0;
+
+         titleEdited = title.value
+         contentEdited = content.value
+
+         replaceTag(title, 'div')
+         replaceTag(content, 'div')
+
+         editButton.textContent = 'Edit'
+
+         for (let item of taskList) {
+            if (sticker.dataset.id == item.id) {
+               if (titleEdited && contentEdited) {
+                  stickerSetter(item, 'title', titleEdited)
+                  stickerSetter(item, 'content', contentEdited)
+               }
+            }
+         }
+
+         setDataToLocalStorage()
+         showLocalData(taskList)
+      }
+
+   }
+
+
    // Data Block
+
+   function replaceTag(target, tag) {
+      target.outerHTML = `<${tag} class="${target.classList}">` + target.innerHTML + `</${tag}>`
+   }
 
    function showLocalData(localData) {
       nodeForStickers.innerHTML = '';
@@ -44,13 +117,12 @@ window.onload = () => {
       if (localData) {
          for (let item of localData) {
             if (item.class == "Sticker") {
-               if (!stickerArray.includes(setStickerDataToHTML(item.id))) {
+               if (!stickerArray.includes(setStickerDataToHTML(item))) {
                   stickerArray.push(setStickerDataToHTML(item))
                }
             }
          }
       }
-
 
       for (let item of stickerArray) { //Render stickers
          nodeForStickers.insertAdjacentHTML('beforeend', item)
@@ -61,6 +133,13 @@ window.onload = () => {
             deleteSticker(button.closest('.sticker'))
          })
       }
+
+      for (let button of document.querySelectorAll(".sticker__edit")) {
+         button.addEventListener('click', () => {
+            editSticker(button.closest('.sticker'))
+         })
+      }
+
    }
 
    function setDataToLocalStorage() {
